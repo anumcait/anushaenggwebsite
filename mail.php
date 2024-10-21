e6edb952537c69fdbebf5d379379e91cd3a2624b
Anu <anumcait@gmail.com>
2024-10-21 11:52:29 +0530

    latest

--- mail.php
+++ mail.php
@@ -1,21 +1,25 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Capture form data
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $phone = htmlspecialchars(trim($_POST['phone']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
 
     // Your email address where you want to receive the messages
-    $to = "anumcait@gmail.com";
-    $subject = "New Message from Contact Form";
+    $to = "anumcait@gmail.com";  // Change this to your email address
+    $subject = "New Message from Contact Form: $subject";  // Email subject
 
     // Build the email content
     $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Phone: $phone\n\n";
     $email_content .= "Message:\n$message\n";
 
     // Email headers
-    $headers = "From: $name <$email>";
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
     // Send the email
     if (mail($to, $subject, $email_content, $headers)) {
